5fc115500976ceca4ab64e804cd3047509b780ed
Vignesh Srinivasa Raghavan <86973154+Akshutechy@users.noreply.github.com>
2023-12-10 21:16:33 +0100

    Framework Setup

---
+++ codecept.conf.js
@@ -0,0 +1,56 @@
+exports.config = {
+  output: './output',
+  helpers: {
+    Playwright: {
+      browser: 'chromium',
+      url: 'https://www.oranum.com/',
+      show: false
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  mocha: {},
+  bootstrap: null,
+  timeout: 60,
+  teardown: null,
+  hooks: [],
+  gherkin: {
+    features: './tests/features/*.feature',
+    steps: './tests/step_definitions/*.js',
+  },
+  plugins: {
+    screenshotOnFail: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    retryFailedStep: {
+      enabled: true
+    },
+    retryTo: {
+      enabled: true
+    },
+    eachElement: {
+      enabled: true
+    },
+    pauseOnFail: {},
+    allure:{
+      enabled:true,
+      require: '@codeceptjs/allure-legacy'
+    }
+  },
+  stepTimeout: 0,
+  stepTimeoutOverride: [{
+      pattern: 'wait.*',
+      timeout: 0
+    },
+    {
+      pattern: 'amOnPage',
+      timeout: 0
+    }
+  ],
+  tests: './*_test.js',
+  name: 'Akshaya-Byborg-Test-Automation-Engineer-Assignment'
+}
